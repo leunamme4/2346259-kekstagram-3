b1bd5ee1fc0776e0409710e91e4d77108ecf91ff
leunamme4 <90042349+leunamme4@users.noreply.github.com>
2023-05-11 02:03:47 +0300

    module 8 task 1

---
+++ js/form.js
@@ -0,0 +1,23 @@
+function checkForm() {
+  const form = document.querySelector('.img-upload__form');
+
+  const pristine = new Pristine({
+    classTo: 'form__item',
+    errorClass: 'form__item--invalid',
+    successClass: 'form__item--valid',
+    errorTextParent: 'form__item',
+    errorTextTag: 'span',
+    errorTextClass: 'form__error'
+  });
+  function validateComment (value) {
+    return value.length >= 20 && value.length <= 140;
+  }
+
+  pristine.addValidator(
+    form.querySelector('.text__description'),
+    validateComment,
+    'От 20 до 140 символов'
+  );
+}
+
+export {checkForm};
