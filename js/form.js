3322293f3ef9bfc9c0b0d817f92789cf8e490c13
leunamme4 <90042349+leunamme4@users.noreply.github.com>
2023-05-11 14:30:12 +0300

    .

--- js/form.js
+++ js/form.js
@@ -1,3 +1,7 @@
+const imageUpload = document.querySelector('.img-upload__overlay');
+const fileInput = document.querySelector('.img-upload__input');
+const closeImageUploadButton = document.querySelector('.img-upload__cancel');
+
 function checkForm() {
   const form = document.querySelector('.img-upload__form');
 
@@ -20,4 +24,23 @@ function checkForm() {
   );
 }
 
-export {checkForm};
+function escapeKeyHandler(ev) {
+  if (ev.key === 'Escape') {
+    closeImageUpload();
+  }
+}
+
+function closeImageUpload() {
+  imageUpload.classList.add('hidden');
+  document.removeEventListener('keydown', escapeKeyHandler);
+}
+
+function openImageUpload() {
+  imageUpload.classList.remove('hidden');
+  document.addEventListener('keydown', escapeKeyHandler);
+}
+
+fileInput.addEventListener('change', openImageUpload);
+closeImageUploadButton.addEventListener('click', closeImageUpload);
+
+export {checkForm, closeImageUpload};
